a00fcb09222073483a24f6eb77bbe76e6de554d4
Vimal-Mahendran <151828616+Vimal-Mahendran@users.noreply.github.com>
2024-06-12 13:03:17 +0530

    5. Combination of sum.py

--- day 6 -lab/5. Combination of sum.py
+++ day 6 -lab/5. Combination of sum.py
@@ -12,7 +12,6 @@ def combinationSum(candidates, target):
     backtrack(0, target, [])
     return result
 
-# Example usage:
 candidates = [2, 3, 6, 7]
 target = 7
-print(combinationSum(candidates, target))  # Output: [[2, 2, 3], [7]]
+print(combinationSum(candidates, target))
